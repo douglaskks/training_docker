7f415c14171fc5a1776e2aa9ad32ee5f2f375d6f
douglaskks <douglaszxv2@gmail.com>
2023-03-03 20:43:34 +0000

    efs + doker

---
+++ user_data.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+yum update -y
+yum install -y httpd
+yum install yum remove docker \
+                docker-client \
+		docker-client-latest \
+		docker-common \
+		docker-latest \
+		docker-latest-logrotate \
+		docker-logrotate \
+		docker-engine \
+		podman \
+		runc
+yum install -y yum-utils
+yum-config-manager \
+	--add-repo \
+	https://download.docker.com/linux/rhel/docker-ce.repo
+yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+yum install -y git
+curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o docker-compose
+mv docker-compose /usr/local/bin && sudo chmod +x /usr/local/bin/docker-compose
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
